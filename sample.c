16813c28839e6a7addc405b42dc24043802a88b6
psdhanesh7 <psdhanesh2000@gmail.com>
2020-04-06 12:54:11 +0530

    Conflict resolved

--- sample.c
+++ sample.c
@@ -5,6 +5,14 @@ void main() {
 	printf("Hello World\n");
 	printf("Hello World\n");
 	printf("Hello World\n");
+<<<<<<< HEAD
 	// Comment
 }
 //Content to append
+=======
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+}
+>>>>>>> anotherBranch
